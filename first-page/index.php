d842e82bb72a16f175f921c0029c90eaa1c74f12
naveen kumar <nkgitm@gmail.com>
2021-07-06 11:03:26 +0530

    this is initial page

---
+++ first-page/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <meta  charset=utf-8" >
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title > THIS IS WELCOME PAGE MY FRIENDS BY NAVEEN KUMAR</title>
+    </head>
+
+   </body>
+<h6>HELLO WORLD </h6>
+   </body>
+</html>   
